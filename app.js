b7b85359007bcd1220d80e9db7fad0c802b63f58
mbonubasil <mbonubasil@gmail.com>
2017-08-22 03:03:42 +0100

    make search query now dynamic

--- app.js
+++ app.js
@@ -5,17 +5,17 @@ const express = require('express');
 const request = require('request-promise');
 
 
-
 const app = express();
 
 
-app.get('/snow', (req, res) => {
+
+app.get('/api/:title', (req, res) => {
 
 	request({
 		uri: 'https://api.themoviedb.org/3/search/movie',
 		qs: {
 			api_key: process.env.API_KEY,
-			query: 'Snow'
+			query: req.params.title
 		},
 		json: true
 	})
