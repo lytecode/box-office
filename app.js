71d05ee0fcff6cf7157932b222e41f90edb8ca9c
mbonubasil <mbonubasil@gmail.com>
2017-08-22 17:58:30 -0400

    complete api

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const request = require('request-promise');
 const app = express();
 
 
-
-app.get('/api/:title', (req, res) => {
+//query the for movies using the movie title
+app.get('/api/search/:title', (req, res) => {
 
 	request({
 		uri: 'https://api.themoviedb.org/3/search/movie',
@@ -20,7 +20,28 @@ app.get('/api/:title', (req, res) => {
 		json: true
 	})
 	.then((data) => {
-		console.log(data)
+		console.log("Hello, This is the search route and you searched for movie " +req.params.title );
+	})
+	.catch((err) => {
+		console.log(err)
+	})
+
+});
+
+
+//query the api by movie category
+app.get('/api/load/:category', (req, res) => {
+
+	request({
+		uri: 'https://api.themoviedb.org/3/search/movie',
+		qs: {
+			api_key: process.env.API_KEY,
+			query: req.params.category
+		},
+		json: true
+	})
+	.then((data) => {
+		console.log("Hello, this is the load route, and you loaded movies in category " +req.params.category);
 	})
 	.catch((err) => {
 		console.log(err)
